4e897f6dc84f25d0383dd1a57edb22b1d36ad59c
Bruce Robinson <3518111154@uth.edu.mx>
2020-03-12 09:15:46 -0600

    El web service login.php ahora devuelve los datos del usuario y la conexión no muestra mensajes de conexión exitosa o fallida

--- login.php
+++ login.php
@@ -1,17 +1,33 @@
 <?php
   include_once 'conection.php';
 
+  //Recibir datos POST
+  $data = json_decode(file_get_contents('php://input'),true);
+  $usuario = $data['usuario'];
+  $password = $data['password'];
+
   //Crear un objeto de tipo conexión
   $mongo = getConnection();
 
   //Consulta
-  $filtros=['ciudad'=>'Puebla']; //$filtros=['nombre'=>'valor_atributo'];
-  $opciones=[];
-  $query=new MongoDB\Driver\Query($filtros,$opciones);
-  $cursor=$mongo->executeQuery('db_domotica.domicilio',$query);
+  $filtros = ['usuario'=>''.$usuario.'', 'password'=>''.$password.'']; 
+      //print_r($filtros); //PRUEBAS
+  $opciones = [];
+  $query = new MongoDB\Driver\Query($filtros,$opciones);
+  $cursor = $mongo->executeQuery('bd_domotica_divided.usuario',$query);
 
   foreach($cursor as $row){
-    print_r($row);
+    $row;
   }
   
+  if($row != null ){ 
+    $userData = json_decode(json_encode($row), true);
+    header("HTTP/1.1 200 OK");
+    echo json_encode($userData);
+  }
+  else {
+    header("HTTP/1.1 401 Unauthorized");
+    echo json_encode(array("mensaje"=>"Datos de acceso incorrectos"));
+  }
+
 ?>
